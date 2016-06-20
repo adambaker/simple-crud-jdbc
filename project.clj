a263db12663ce1dba258f4ca16c06faffbe0da8e
Adam C. Baker <adam.durandal@gmail.com>
2016-06-19 23:39:16 -0700

    dependencies, metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject simple-crud-jdbc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :url "https://github.com/adambaker/simple-crud-jdbc"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.6.1"]]
+
+  :min-lein-version "2.0.0"
+  :profiles {:dev {:dependencies [[org.postgresql/postgresql "9.4-1208"]
+                                  [environ "1.0.3"]]}}
+  )
