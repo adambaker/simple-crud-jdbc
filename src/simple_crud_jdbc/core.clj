516794fd9c5277173afc92ea18fecd5e93234f98
Adam C. Baker <adam.durandal@gmail.com>
2016-06-27 14:57:39 -0700

    first tests for connection bound methods.

--- src/simple_crud_jdbc/core.clj
+++ src/simple_crud_jdbc/core.clj
@@ -1,6 +1,61 @@
-(ns simple-crud-jdbc.core)
+(ns simple-crud-jdbc.core
+  (:require [honeysql.core :as sql]
+            [clojure.java.jdbc :as jdbc])
+  (:import [clojure.lang IPersistentMap IPersistentVector]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn- add-where-item [where-vec [k v]] (conj where-vec [:= k v]))
+(defn where-map [where]
+  (cond
+    (and (instance? IPersistentMap where) (seq where))
+      (reduce add-where-item [:and] (seq where))
+    (instance? IPersistentVector where) where
+    :default true))
+
+(defn- method-opts [opt]
+  (cond
+    (map? opt) [(or (opt :var) (throw (IllegalArgumentException.
+                                        "Metadata must have a :var key")))
+                (dissoc opt :var)]
+    (symbol? opt) [opt {}]
+    :default (throw (IllegalArgumentException.
+                      (str "Method value must be a symbol, a metadata map, "
+                           "or nil/false")))))
+(def ^:private default-opts {:read-one 'read-one, :read-all 'read-all
+                             :create 'create!, :connection nil})
+(defn- with-conn [conn opts [args :as impl]]
+  (let [[sym md] (method-opts opts)
+        unbound-def `(defn ~sym ~md ~impl)]
+    (if conn
+      (let [bound-args (vec (rest args))
+            bound-impl `(~bound-args (~sym ~conn ~@bound-args))]
+        (concat unbound-def [bound-impl]))
+      unbound-def)))
+
+(defmacro crud-for [table & [opts]]
+  (assert (instance? clojure.lang.Keyword table))
+  (let [{:keys [read-one read-all create connection]} (merge default-opts opts)]
+    `(do
+       ~(when read-one
+          (with-conn connection read-one
+            `([conn# where#]
+              (->> {:select [:*] :from [~table] :where (where-map where#)
+                    :limit 1}
+                   sql/format
+                   (jdbc/query conn#)
+                   first))))
+       ~(when read-all
+          (with-conn connection read-all
+            `([conn# where#]
+              (->> {:select [:*] :from [~table] :where (where-map where#)}
+                   sql/format
+                   (jdbc/query conn#)))))
+       ~(when create
+          (with-conn connection create
+            `([conn# attrs#] (first (jdbc/insert! conn# ~table attrs#)))))
+       )))
+
--- test/simple_crud_jdbc/core_test.clj
+++ test/simple_crud_jdbc/core_test.clj
@@ -1,7 +1,63 @@
 (ns simple-crud-jdbc.core-test
   (:require [clojure.test :refer :all]
-            [simple-crud-jdbc.core :refer :all]))
+            [environ.core :refer [env]]
+            [clojure.java.jdbc :as jdbc]
+            [conman.core :refer [connect! disconnect! with-transaction]]
+            [simple-crud-jdbc.core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^:dynamic *db* nil)
+
+(use-fixtures :once
+  (fn [tests]
+    (binding [*db* (connect! {:jdbc-url (env :db-url)})]
+      (jdbc/db-do-commands *db*
+                           (jdbc/create-table-ddl :fruit
+                                                  [[:id "serial" :primary :key]
+                                                   [:name "varchar(32)"]
+                                                   [:appearance "varchar(32)"]
+                                                   [:cost :int]]))
+      (tests)
+      (jdbc/db-do-commands *db* (jdbc/drop-table-ddl :fruit))
+      (disconnect! *db*))))
+
+(create-ns 'default-conn)
+(create-ns 'default-no-conn)
+(in-ns 'default-conn)
+(simple-crud-jdbc.core/crud-for
+  :fruit {:connection simple-crud-jdbc.core-test/*db*})
+(in-ns 'default-no-conn)
+(simple-crud-jdbc.core/crud-for :fruit)
+
+(in-ns 'simple-crud-jdbc.core-test)
+
+(use-fixtures :each (fn [tests]
+                     (with-transaction [*db*]
+                       (jdbc/db-set-rollback-only! *db*)
+                       (tests))))
+
+(deftest default-conn-create-read!
+  (let [orange (default-conn/create!
+                 {:name "orange", :appearance "orange", :cost 120})
+        pear (default-conn/create!
+               {:name "pear", :appearance "pear shaped", :cost 230})
+        grape (default-conn/create!
+                {:name "grape", :appearance "purple", :cost 210})
+        grapefruit (default-conn/create!
+                     {:name "grapefruit", :appearance "grapefruit shaped"
+                      :cost 100})]
+    (is (= (:count (first (jdbc/query *db* "select count(*) from fruit")))
+           4))
+    (is (= (default-conn/read-one {:id (orange :id)})
+           orange))
+    (is (= (set (default-conn/read-all [:or
+                                        [:like :name "grape%"]
+                                        [:like :appearance "%shaped"]]))
+           #{pear grape grapefruit}))
+
+    ;call in different txn, returns nil
+    (is (= (default-conn/read-one {:connection-uri (env :db-url)}
+                                  {:id (orange :id)})
+           nil) "can still specify a connection to override default")
+    ))
+
+
